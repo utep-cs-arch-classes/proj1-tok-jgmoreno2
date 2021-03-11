ad7dbe40edbdcf1ff42ef0b532eba0950a59c591
Jacob Moreno <jgmoreno2@miners.utep.edu>
2021-03-10 17:11:44 -0700

    Final changes made. Fixed interface to suit the needs of the project.

--- src/history.c
+++ src/history.c
@@ -12,11 +12,11 @@ void add_history(List *list, char *str){
   item->next = NULL;
   Item *ptr = list->root;
   if(ptr == NULL){
-    item->id = 0;
+    item->id = 1;
     list->root = item;
   }
   else{
-    int currId = 0;
+    int currId = 2;
     while(ptr != NULL && ptr->next != NULL){
       currId++;
       ptr = ptr->next;
@@ -36,12 +36,22 @@ char *get_history(List *list, int id){
 
 void print_history(List *list){
   Item *ptr = list->root;
-  do{
-    printf("%d: %s\n", ptr->id, ptr->str);
+  while(ptr != NULL){
+    printf("%d: %s\n", ptr->id, get_history(list, ptr->id));
     ptr = ptr->next;
-  }while(ptr != NULL && ptr->next != NULL);
+    if(ptr == NULL){
+      break;
+    }
+  }
 }
 
 void free_history(List *list){
-  
+  Item *ptr = list->root;
+  while(ptr != NULL){
+    Item *last = ptr;
+    ptr = ptr->next;
+    free(last->str);
+    free(last);
+  }
+  free(list);
 }
--- src/interface.c
+++ src/interface.c
@@ -1,29 +1,71 @@
 #include "tokenizer.c"
 #include "history.c"
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 void main(){
-  printf("> ");
-  List *list = init_history();
-  char arr[100];
-  char input;
-  int i = 0;
-  while(i < 100){
-    input = getchar();
-    if(input != '\n'){
-      arr[i] = input;
-      i++;
+  List *hist = init_history();
+  while(1 == 1){
+    printf("Type string to be tokenized, \"!<id>\" to tokenize something from history, or \"!!\" to exit.\n> ");
+    char *str = (char *)malloc(sizeof(char *) * 100);
+    char input;
+    int i = 0;
+    int startsWithExclamation = 0;
+    int id = 0;
+    int isValid = 1;
+    while(1 == 1){
+      input = getchar();
+      if(isValid == 0){
+	if(input == '\n'){
+	  break;
+	} else{
+	  continue;
+	}
+      }
+      if(startsWithExclamation == 1){
+	if(i == 1 && input == '!'){
+	  free_history(hist);
+	  return;
+	} else{
+	  if(isdigit(input)){
+	    printf("%c isdigit", input);
+	    id *= 10;
+	    id += (input - '0');
+	  } else if(input == '\n'){
+	    break;
+	  }else{
+	    printf("\nInvalid id.");
+	    isValid = 0;
+	  }
+	}
+      } else{
+	if(i == 0 && input == '!'){
+	  startsWithExclamation = 1;
+	}
+	if(input != '\n'){
+	  str[i] = input;
+	  i++;
+	} else{
+	  str[i] = '\0';
+	  break;
+	}
+      }
     }
-    else{
-      arr[i] = '\0';
-      break;
+    if(isValid == 0){
+      goto fin;
     }
+    if(id != 0){
+      str = get_history(hist, id);
+    }
+    add_history(hist, &str[0]);
+    char **toks = tokenize(&str[0]);
+    printf("\nTokenizing \"%s\"...\n", str);
+    print_tokens(toks);
+    free_tokens(toks);
+  fin:
+    printf("\nHistory:\n");
+    print_history(hist);
+    printf("\n\n");
   }
-  add_history(list, &arr[0]);
-  printf("Tokenizing \"%s\"...\n", arr);
-  char **toks = tokenize(&arr[0]);
-  print_tokens(toks);
-  free_tokens(toks);
-  print_history(list);
-  return;
 }
